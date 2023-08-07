6b227401b290efab6c1a80b8e8fabbade39658d6
Duncan Joly <duncanjoly13@gmail.com>
2023-08-07 18:26:38 -0500

    added user input, tested extraction

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # takeout automation
- 
+
+# Usage
+All takeout .zip files should be placed in an empty directory with the .py file 
 # requirements
-## No external prerequisites
+No external prerequisites
 * glob
-* sys
+* os
 * zipfile
--- takeout.py
+++ takeout.py
@@ -1,32 +1,87 @@
-import glob, sys, zipfile
+import glob, os, zipfile
 
-class Migrate(): 
-
+class Migrate():
     def __init__(self):
         self.output_folder_path = r'./MOVE_ME/'
-        self.source_folder_path = r'./ZIPS_HERE/'
-        self.all_zip_paths = glob.glob(str(self.source_folder_path + '*.zip'))
+        self.all_zip_paths = glob.glob('*.zip')
+        print('zip files to be extracted:')
+        print(self.all_zip_paths)
+        print()
 
     def run(self):
+        # check for zips
+        if self.all_zip_paths == []:
+            print('no .zip files found!')
+            print('ensure that your takeout .zip files are in this directory')
+            print()
+            no_zips_user_input = input('press "q" then "Enter" to exit\n')
+            print()
+            while no_zips_user_input != 'q':
+                print('invalid input!')
+                print()
+                no_zips_user_input = input('press "q" then "Enter" to exit\n')
+                print()
+            if no_zips_user_input == 'q':
+                return
+        
+        # check for output folder
+        if os.path.isdir(self.output_folder_path):
+            print('WARNING: output folder ' + self.output_folder_path + ' exists!')
+            folder_exists_user_input = input('press "q" then "Enter" to quit or "a" then "Enter" to proceed\n')
+            print()
+            while (folder_exists_user_input != 'q') and (folder_exists_user_input != 'a'):
+                print('invalid input!')
+                print()
+                folder_exists_user_input = input('press "q" then "Enter" to quit or "a" then "Enter" to proceed\n')
+            if folder_exists_user_input == 'q':
+                return
+            if folder_exists_user_input == 'a':
+                print('proceeding...')
+                print()
+
         for zip_path in self.all_zip_paths:
             print('extracting ' + zip_path + '...')
             new_zip_obj = zipfile.ZipFile(zip_path)
             new_zip_obj.extractall(path = self.output_folder_path)
             new_zip_obj.close()
             print(zip_path + ' completed, moving on...')
+            print()
 
-        print('all files from ' + self.source_folder_path + ' extracted to ' + self.output_folder_path + ', rearranging folder structure')
+        print('all .zip files in the current directory have been extracted to ' + self.output_folder_path + ', rearranging folder structure')
+        print()
 
         # DELETE UNIMPORTANT FOLDERS WHILE RETAINING ALBUM NAMES (e.g. 'Photos from 2023')
         # check if contains non paths and non .jsons
 
         print('folders rearranged, deleting .json files')
+        print()
 
         # DELETE ALL .json FILES FROM ALL DIRECTORIES
 
-        print('completed')
-        print('your files may be found in ' + self.output_folder_path + ', either rename that folder and move it to the directory of your choice \
-            (e.g. C://Users/YOUR_USERNAME/Pictures/) or drag its contents to a directory of your choice if you\'ve done this before')
+        print('completed!')
+        print()
+        print('would you like to delete the zip files as they are no longer needed?')
+        delete_zips_user_input = input('press "y" then "Enter" to delete the unneeded files or "n" then "Enter" to leave them alone\n')
+        print()
+        while (delete_zips_user_input != 'n') and (delete_zips_user_input != 'y'):
+            print('invalid input!')
+            delete_zips_user_input = input('press "y" then "Enter" to delete the unneeded files or "n" then "Enter" to leave them alone\n')
+        if delete_zips_user_input == 'n':
+            print('your files may be found in ' + self.output_folder_path + ', either rename that folder and move it to the directory of your choice' + \
+                '(e.g. C://Users/YOUR_USERNAME/Pictures/) or drag its contents to a directory of your choice if you\'ve done this before')
+            print('exiting...')
+        if delete_zips_user_input == 'y':
+            for zip_file in self.all_zip_paths:
+                if os.path.isfile(zip_file):
+                    print('deleting ' + zip_file)
+                    os.remove(zip_file)
+            print()
+            print('all zip files have been deleted')
+            print()
+            print('your files may be found in ' + self.output_folder_path + ', either rename that folder and move it to the directory of your choice' + \
+                '(e.g. C://Users/YOUR_USERNAME/Pictures/) or drag its contents to a directory of your choice if you\'ve done this before')
+            print('exiting...')
+
         
 if __name__ == '__main__':
     migration = Migrate()
